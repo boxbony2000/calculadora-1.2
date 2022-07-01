e3914b71935ce2e4b2fa29c485e464eefd7f7a80
luisangel2000 <luisangelsqgredoramirez342@gmail.com>
2022-07-01 17:27:32 -0500

    botones 1.2

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const elementos = ['²','Pi','^','!','AC', '()', '%', '÷', 7,8,9, '×', 4,5,6, '-', 1,2,3, '+', 0, '.','⌫','=' ]
+  const nombres=['Daniela', 'Luis', 'Beto', 'Guicho', 'Susana']
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View stile= {styles.elementos}>
+        
+      </View>   
+      <View style={styles.buttons}>
+      {
+            elementos.map(elemento=>{
+                return(
+                    <TouchableOpacity
+                    key={elemento}>
+                    <View style={styles.button} >
+                    <Text style={styles.texto}>{elemento}</Text>
+                    </View>
+                    </TouchableOpacity>
+              
+                );
+            })
+        }
+        </View>
+  
+
+
     </View>
   );
 }
@@ -13,8 +36,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    color: "#ffffff",
+    fontSize:45,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    borderColor: '#3f4d5b',
+    boderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: 90,
+    minHeigt: 90,
+    flex: 2,
+  },
 });
+
